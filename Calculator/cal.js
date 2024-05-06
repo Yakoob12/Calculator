0e20acf40aa645707ba2d328bfbf48c1e52c82b5
Yakoob12 <162607020+Yakoob12@users.noreply.github.com>
2024-05-06 23:40:35 +0530

    Add files via upload

---
+++ Calculator/cal.js
@@ -0,0 +1,24 @@
+let outputscreen = document.getElementById("output-screen");
+
+
+function display(num){
+    outputscreen.value +=num;
+}
+function calculate(){
+    try{
+        outputscreen.value = eval(outputscreen.value);
+
+
+    }
+    catch(err){
+            alert("Invalid");
+    }
+}
+function Clear(){
+    outputscreen.value = "";
+}
+function Del(){
+    outputscreen.value = outputscreen.value.slice(0,-1);
+}
+
+
